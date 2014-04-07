3364c9f9be8787fc8f2b209b9ffa987960f08b9c
coltonmorris <coltonmorris94@gmail.com>
2014-04-07 16:26:35 -0600

    Testing php

---
+++ index.php
@@ -0,0 +1,15 @@
+<HTML>
+	<head>Colton's Website</head>
+		<body>
+			<div>
+				Hello World!
+			</div>
+			<div>
+				This will now test php!
+				<?php 
+				echo "<br /";
+				echo "hello there";
+				?>
+			</div>
+		</body>
+</HTML>
---
+++ makefile
@@ -0,0 +1,3 @@
+run:
+	git commit -am "Testing php"
+	git push
